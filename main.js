2dd7acaae586ac2bde012287c2b821cc1502346e
Mr Unknown <kalattas66@gmail.com>
2021-11-14 15:16:35 +0000

    Update main.js

--- main.js
+++ main.js
@@ -48,45 +48,45 @@ function togglePass(){
 }
 
 // Form Validation
-
-function checkStuff() {
-  let status=0;  
-  var email = document.form1.email;
-  var password = document.form1.password;
-  var msg = document.getElementById('msg');
-  
-  if (email.value == "") {
-    msg.style.display = 'block';
-    msg.innerHTML = "Please enter your email";
-    email.focus();
-    return false;
-  } else {
-    //msg.innerHTML = "";---------------------------------------------------
-    status++;
-  }
-  
-   if (password.value == "") {
-    msg.innerHTML = "Please enter your password";
-    password.focus();
-    return false;
-  } else {
-    //msg.innerHTML = "";---------------------------------------------------
-    status++;
-  }
-   var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if (!re.test(email.value)) {
-    msg.innerHTML = "Please enter a valid email";
-    email.focus();
-    return false;
-  } else {
-    //msg.innerHTML = "";------------------------------------------------------
-    status++;
-  }
-  console.log(status)
-  if(status==3){
-      windows.close();
-      window.open("main_screen.html", "_blank");
-  }
-}
-
-
+//
+//function checkStuff() {
+//  let status=0;  
+//  var email = document.form1.email;
+//  var password = document.form1.password;
+//  var msg = document.getElementById('msg');
+//  
+//  if (email.value == "") {
+//    msg.style.display = 'block';
+//    msg.innerHTML = "Please enter your email";
+//    email.focus();
+//    return false;
+//  } else {
+//    //msg.innerHTML = "";---------------------------------------------------
+//    status++;
+//  }
+//  
+//   if (password.value == "") {
+//    msg.innerHTML = "Please enter your password";
+//    password.focus();
+//    return false;
+//  } else {
+//    //msg.innerHTML = "";---------------------------------------------------
+//    status++;
+//  }
+//   var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//  if (!re.test(email.value)) {
+//    msg.innerHTML = "Please enter a valid email";
+//    email.focus();
+//    return false;
+//  } else {
+//    //msg.innerHTML = "";------------------------------------------------------
+//    status++;
+//  }
+//  console.log(status)
+//  if(status==3){
+//      windows.close();
+//      window.open("main_screen.html", "_blank");
+//  }
+//}
+//
+//
