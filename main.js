2096282e055145444e743e644c6c81d56bb9362f
Antonios Kalattas <kalattas66@gmail.com>
2023-09-03 21:12:57 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,36 +1,95 @@
-const {spawn} = require('child_process');
-const bootstrap = require('bootstrap')
-let elements_to_find=0;
+const { spawn } = require('child_process');
+//const bootstrap = require('bootstrap')
+let elements_to_find = 0;
+//https://www.ucy.ac.cy/aasw/studies/undergraduate-studies/schedule-classes/
+function exec_py() {
+    const pyscript = spawn('python', ['main_script.py', elements_to_find, url, element]);
 
-function exec_py(){
+    pyscript.stdout.on('data', (data) => {
+        console.log(`output: ${data}`);
+    });
 
-  let url = document.getElementById("url").value;
-  let element = [];
+    pyscript.stderr.on('data', (data) => {
+        console.error(`error: ${data}`);
+    });
 
-  for(let i =0; i<elements_to_find;i++){
-    element[i]=document.getElementById(`s${i+1}`).value;
-    console.log(element[i])
-  }
-  const pyscript = spawn('python', ['main_script.py', elements_to_find, url, element]);
-
-  pyscript.stdout.on('data', (data)=>{
-     console.log(`stdot: ${data}`);
-   });
-
-  pyscript.stderr.on('data', (data)=>{
-       console.error(`stderr: ${data}`);
-   });
-
-  pyscript.on('close', (code) =>{
-     console.log(`script exited with code ${code}`);
-   });
+    pyscript.on('close', (code) => {
+        console.log(`script exited with code ${code}`);
+    });
 }
 
 function add_element(){
   elements_to_find++;
   const li = document.createElement('li');
-  li.innerHTML = `<input id="s${elements_to_find}" class="text-box" type="text" placeholder="elements to search"><br>`;
+  li.id=elements_to_find
+  li.innerHTML = `<a id=${elements_to_find} class="fill-in"><input id="s${elements_to_find}" class="text-box" type="text" placeholder="elements to search"><br>
+  <button id="option_btn${elements_to_find}" onclick=option("option_btn${elements_to_find}")>option</button>
+  <button onclick=Delete1(${elements_to_find})>delete</button></button></a>`;
   document.querySelector('.add').appendChild(li);
+}
+
+//when clicking on the button options
+function option(id){
+  element = document.getElementById(id);
+  value=document.getElementById(id).textContent;
+  if(value=="option"){
+    element.textContent="Click";
+    const ul = document.createElement('ul');
+    //button for click
+    ul.innerHTML=`<li><input id= "c${elements_to_find}" type="number" name="number of clicks" min="1" max="1000" onclick="stopPropagation(event)"></li>`;
+    document.querySelector(`#${id}`).appendChild(ul);
+  }
+  else if(value=="Click"){
+    element.textContent="Type";
+    const ul = document.createElement('ul');
+    //button for typing
+    ul.innerHTML=`<li><input id="t${elements_to_find}" class="type_box" type="text" placeholder="what to type" onclick="stopPropagation(event)"><br></li>`;
+    document.querySelector(`#${id}`).appendChild(ul);
+  }
+  else if(value=="Type"){
+    element.textContent="Click";
+    const ul = document.createElement('ul');
+    //button for click
+    ul.innerHTML=`<li><input id="c${elements_to_find}" type="number" name="number of clicks" min="1" max="1000" onclick="stopPropagation(event)"></li>`;
+    document.querySelector(`#${id}`).appendChild(ul);
+  }
+  
+}
+
+//to stop a click on a button when trying to input data to change
+function stopPropagation(event) {
+  event.stopPropagation();
+}
+
+//to remove an input of an element
+function Delete1(id){
+  elements_to_find--;
+    element= document.getElementById(id)
+    element.remove()
+
+}
+
+function collect_data(){
+  let url = document.getElementById("url").value;
+    let element = [];
+    let type =[];
+    let clicks=[];
+
+    //grap the element path
+    for (let i = 0; i < elements_to_find; i++) {
+      console.log(type , clicks)
+        element[i] = document.getElementById(`s${i+1}`).value;
+        //split the elements
+        element[i] += "!__!";  
+        if(document.getElementById("c"+[i]))
+          clicks[i] = document.getElementById("c"+[i]).textContent;
+          if(document.getElementById("c"+[i])==false){
+          type[i] = document.getElementById("t"+[i]).textContent;
+          console.log("in");
+          }
+    }
+    console.log(type , clicks)
+    //exec_py(url, element)
 }
 
 //btn.addEventListener("click", exec_py);
